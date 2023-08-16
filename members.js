a577b3a43ac3006497f3693e005ba66253b10568
SJashwanth <73937697+SJashwanth@users.noreply.github.com>
2023-08-16 14:21:41 +0000

    Copilot second commit

--- members.js
+++ members.js
@@ -1,12 +1,14 @@
-function skillsmembers() {
-    var members = document.getElementById("members");
-    var skills = document.getElementById("skills");
-    var membersbutton = document.getElementById("membersbutton");
-    var skillsbutton = document.getElementById("skillsbutton");
-    members.style.display = "block";
-    skills.style.display = "none";
-    membersbutton.style.backgroundColor = "#f5f5f5";
-    membersbutton.style.color = "#000000";
-    skillsbutton.style.backgroundColor = "#000000";
-    skillsbutton.style.color = "#ffffff";
+function skillsmember(){
+    return {
+        name: 'skillsmember',
+        path: '/skillsmember',
+        component: {
+            template: `
+            <div>
+            <h1>Skills Member</h1>
+            <p>Ini adalah halaman Skills Member</p>
+            </div>
+            `
+        }
+    }
 }
